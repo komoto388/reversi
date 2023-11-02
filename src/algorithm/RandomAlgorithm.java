52249823bdfedc8e5519c0d9a09036d66dda47fc
komoto <146818126+komoto388@users.noreply.github.com>
2023-11-02 21:37:16 +0900

    Algorithmクラスの修正
    * Algorithmクラスにて評価値のデフォルト値・最小値・最大値を定数として定義した。
    * 内部クラス Evaluateクラスのコンストラクタの引数から、デフォルト値の設定を削除し、定数 DEFAULT_POINT の値 (0) を設定するようにした。
    * 変数名を変更した。

--- src/algorithm/RandomAlgorithm.java
+++ src/algorithm/RandomAlgorithm.java
@@ -7,18 +7,21 @@ import reversi.Dimension;
  * ランダムに石を置くアルゴリズム
  */
 public class RandomAlgorithm extends Algorithm {
+    
+    /**
+     * 設置可能なマスに対して、ランダムに石を置くアルゴリズム
+     */
     @Override
     public Dimension run(Board board, Boolean playerIsBlack) {
         final Dimension boardSize = board.getSize();
-        Evaluate evaluate = new Evaluate(boardSize, 0);
+        Evaluate evaluate = new Evaluate(boardSize);
 
         for (int i = 0; i < boardSize.getRow(); i++) {
             for (int j = 0; j < boardSize.getColumn(); j++) {
-                // 石を置けるますの場合は、1～100のランダム値をあたえる。
+                // 石を置けるますの場合は、1～100のランダム値を加える。
                 Dimension target = new Dimension(i, j);
                 if (board.countReversibleDisc(target, playerIsBlack) > 0) {
-                    evaluate.set(target, random.nextInt(100) + 1);
-                    // System.out.printf("設置可能な座標: %s, 評価値: %d\n", target.getString(), evaluate.getValue(target));
+                    evaluate.add(target, random.nextInt(100) + 1);
                 }
             }
         }
